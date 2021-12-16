bdf590225439fbba3d098902795148a356036685
MariaLuiza-CS <m.luiza1843@gmail.com>
2021-12-16 20:52:15 -0300

    Fist changes of login screen, add viewbinding, app's logo, navigation to singup fragment

---
+++ app/src/main/java/com/example/marsapp/SignUpFragment.kt
@@ -0,0 +1,32 @@
+package com.example.marsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.marsapp.databinding.FragmentSingUpBinding
+
+class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSingUpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_sing_up, container, false)
+        binding = FragmentSingUpBinding.bind(view)
+
+        binding.btnSignup.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.signUpFragment_to_loginFragment)
+        }
+
+//        binding.btnViewSignup.setOnClickListener {
+//            Navigation.findNavController(view).navigate(R.id.signUpFragment_to_loginFragment)
+//        }
+        return binding.root
+    }
+
+}
